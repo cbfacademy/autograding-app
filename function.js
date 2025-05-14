f53f212d8e466f37b402aa54a4fa46a1d6a193df
Genyus <genyus@gmail.com>
2025-05-14 17:20:44 -0400

    chore: add more debug output

--- function.js
+++ function.js
@@ -1,15 +1,18 @@
 import { createNodeMiddleware, createProbot } from "probot";
 import app from "./app.js";
-
+console.log('Starting probot app');
 if (process.env.PRIVATE_KEY_BASE64) {
   process.env.PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
 }
-
+console.log('Environment variables set:');
 const middleware = createNodeMiddleware(app, { probot: createProbot() });
-
+console.log('Middleware created');
 export const probotApp = (req, res) => {
+  console.log('Received request');
   middleware(req, res, () => {
+    console.log('Response sent');
     if (!res.headersSent) {
+      console.log('Sending 404 response');
       res.writeHead(404);
       res.end();
     }
