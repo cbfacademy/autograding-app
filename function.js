e59d5a290d1dc9d96fa4aed0c947c528e057c5f6
Genyus <genyus@gmail.com>
2025-05-14 11:24:30 -0400

    fix: set private key before loading probot

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
 import { createNodeMiddleware, createProbot } from "probot";
 import app from "./app.js";
 
-const middleware = createNodeMiddleware(app, { probot: createProbot() });
-
 if (process.env.PRIVATE_KEY_BASE64) {
   process.env.PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
 }
+
+const middleware = createNodeMiddleware(app, { probot: createProbot() });
 
 export const probotApp = (req, res) => {
   middleware(req, res, () => {
