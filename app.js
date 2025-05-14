b6656250040647bb089d3676ed56fc032a03bd60
Genyus <genyus@gmail.com>
2025-05-14 17:35:26 -0400

    chore: change logging method

--- app.js
+++ app.js
@@ -53,14 +53,14 @@ export default (app) => {
   app.on('repository.created', async (context) => {
     const { owner, name } = context.payload.repository;
     const ownerLogin = owner.login;
-    console.log(`Received repository.created for ${ownerLogin}/${name}`);
+    context.log.info(`Received repository.created for ${ownerLogin}/${name}`);
 
     // Get the public key for the repo (needed to encrypt secrets)
     const { data: publicKey } = await context.octokit.actions.getRepoPublicKey({
       owner: ownerLogin,
       repo: name,
     });
-    console.log('Public key retrieved');
+    context.log.info('Public key retrieved');
     // Encrypt the secret value
     const secretValue = process.env.CLASSROOM_TOKEN;
     const key = publicKey.key;
@@ -77,7 +77,7 @@ export default (app) => {
       encrypted_value: encryptedValue,
       key_id: publicKey.key_id,
     });
-    console.log('CLASSROOM_TOKEN secret added');
+    context.log.info('CLASSROOM_TOKEN secret added');
     // Add or update the PR_AGENT_BOT_USER variable
     await createOrUpdateRepoVariable(context.octokit, {
       owner: ownerLogin,
@@ -85,10 +85,10 @@ export default (app) => {
       name: 'PR_AGENT_BOT_USER',
       value: process.env.PR_AGENT_BOT_USER,
     });
-    console.log('PR_AGENT_BOT_USER variable added');
-    console.log(`Added secrets and variables to ${ownerLogin}/${name}`);
+    context.log.info('PR_AGENT_BOT_USER variable added');
+    context.log.info(`Added secrets and variables to ${ownerLogin}/${name}`);
   });
   app.onAny(async (context) => {
-    console.log({ event: context.name, action: context.payload.action });
+    context.log.info({ event: context.name, action: context.payload.action });
   });
 };
