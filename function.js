d758a63a435ea6ab1b0dcc79811717a78e59f8dd
Genyus <genyus@gmail.com>
2025-05-14 11:10:47 -0400

    ci: fix secret key import
    - Uses base64 encoding to prevent multiline private key being imported as multiple environment variables

--- function.js
+++ function.js
@@ -3,6 +3,10 @@ import app from "./app.js";
 
 const middleware = createNodeMiddleware(app, { probot: createProbot() });
 
+if (process.env.PRIVATE_KEY_BASE64) {
+  process.env.PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
+}
+
 export const probotApp = (req, res) => {
   middleware(req, res, () => {
     res.writeHead(404);
